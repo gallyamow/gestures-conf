2a167d559ce1f65fd621fbb3346a426efb8d106c
Ramil Gallyamov <gallyamow@gmail.com>
2020-08-02 17:21:45 +0300

    apply sh

---
+++ apply.sh
@@ -0,0 +1,7 @@
+/opt/libinput-gestures/libinput-gestures-setup stop
+/opt/libinput-gestures/libinput-gestures-setup autostop
+
+cp libinput-gestures.conf ~/.config/libinput-gestures.conf
+
+/opt/libinput-gestures/libinput-gestures-setup start
+/opt/libinput-gestures/libinput-gestures-setup autostart
